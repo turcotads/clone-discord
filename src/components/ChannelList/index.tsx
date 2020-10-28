9e3db34dc1a1d650214330859c130b6e455d4a4a
Fábio Madeira Peres <fabinhomadeira@gmail.com>
2020-10-28 16:55:15 -0300

    channel list created

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Container, Category, AddCategoryIcon } from "./styles";
+
+const ChannelList: React.FC = () => {
+  return (
+    <Container>
+      <Category>
+        <span>Canais de texto</span>
+        <AddCategoryIcon />
+
+        {/* <ChannelButton channelName="chat-livre" />
+      <ChannelButton channelName="trabalho" />
+      <ChannelButton channelName="lolzinho" />
+      <ChannelButton channelName="csgo" />
+      <ChannelButton channelName="valorant" /> */}
+      </Category>
+    </Container>
+  );
+};
+
+export default ChannelList;
