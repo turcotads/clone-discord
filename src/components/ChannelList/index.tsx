9ea8a28db70a26377c9cb8e34ee62e8afe705d8e
Fábio Madeira Peres <fabinhomadeira@gmail.com>
2020-10-28 17:29:54 -0300

    channel button created

--- src/components/ChannelList/index.tsx
+++ src/components/ChannelList/index.tsx
@@ -2,19 +2,21 @@ import React from "react";
 
 import { Container, Category, AddCategoryIcon } from "./styles";
 
+import ChannelButton from "../ChannelButton";
+
 const ChannelList: React.FC = () => {
   return (
     <Container>
       <Category>
         <span>Canais de texto</span>
         <AddCategoryIcon />
+      </Category>
 
-        {/* <ChannelButton channelName="chat-livre" />
+      <ChannelButton channelName="chat-livre" />
       <ChannelButton channelName="trabalho" />
       <ChannelButton channelName="lolzinho" />
       <ChannelButton channelName="csgo" />
-      <ChannelButton channelName="valorant" /> */}
-      </Category>
+      <ChannelButton channelName="valorant" />
     </Container>
   );
 };
